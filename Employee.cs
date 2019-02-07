bafa99e4f28a9f88f92335cd181577291a0b68a1
Krittika Biswas <krittika1302@gmail.com>
2019-02-07 20:02:01 +0530

    C# Assignment 1

---
+++ Employee.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Details
+{
+    class Employee
+    {
+        private int _id;
+        private string _name;
+        private string _departmentname;
+        public Employee()
+        {
+            _id = 1;
+            _name = "";
+            _departmentname = "";
+        }
+        public Employee(int Id, string Name, string DeptName)
+        {
+            Id = _id;
+            Name = _name;
+            DeptName = _departmentname;
+        }
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+
+        public int GetId()
+        {
+            MethodCalled("GetId() Method called.");
+            return Id;
+            
+        }
+        public string GetName()
+        {
+            MethodCalled("GetName() Method called.");
+            return Name;
+            
+        }
+        public string GetDepartmentName()
+        {
+            MethodCalled("GetDepartmentName() Method called.");
+            return DeptName; 
+        }
+        public event CallForDelegate MethodCalled;
+  
+        //Method Overloading
+        public string ForUpdate(int id)
+        {
+            Console.WriteLine("Enter another ID:");
+            string uid = Console.ReadLine();
+            return uid;
+        }
+        public string ForUpdate(string name)
+        {
+            Console.WriteLine("Enter another Name:");
+            string un = Console.ReadLine();
+            return un;
+        }
+        public string ForUpdate()
+        {
+            Console.WriteLine("Enter another Department Name:");
+            string udn = Console.ReadLine();
+            return udn;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Details
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.MethodCalled += new CallForDelegate(MethodCall);
+
+            Console.WriteLine("\nEnter the Id of the Employee: ");
+            emp.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Employee ID is: " + emp.GetId());
+
+            Console.WriteLine("\nEnter the name of the employee: ");
+            emp.Name = Console.ReadLine();
+            Console.WriteLine("Name of the Employee is: " + emp.GetName());
+
+            Console.WriteLine("\nEnter the department of Employee: ");
+            emp.DeptName = Console.ReadLine();
+            Console.WriteLine("The Department of the Employee is: " + emp.GetDepartmentName());
+            Console.WriteLine("10 for Id, 20 for name and 30 for department name \n Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case 10:
+                    string ret = emp.ForUpdate(emp.Id);
+                    Console.WriteLine("The updated Id is: "+ret);
+                    break;
+                case 20:
+                    string ret1 = emp.ForUpdate(emp.Name);
+                    Console.WriteLine("the updated name is :"+ret1);
+                    break;
+                case 30:
+                    string ret2 = emp.ForUpdate(emp.DeptName);
+                    Console.WriteLine("The updated Department Name is: "+ret2);
+                    break;
+                default:
+                    Console.WriteLine("Wrong Choise");
+                    break;
+            }
+        }
+        static void MethodCall(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
